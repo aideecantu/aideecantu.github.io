00c9e811a7d8a07e0af379b2fb2dc94bb14bef88
aideecantu <cantu.aidee@berkeley.edu>
2016-11-28 15:43:00 -0800

    Changes!

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,11 +3,22 @@ $(document).ready(function(){
 $('#about-button').click(function() {
     $('.about-container').fadeIn(1000);
     $('.n-background').hide();
+    $('.contact-container').hide();
+     $('.why-me-container').hide();
   });
 
   $('#contact-button').click(function() {
     $('.contact-container').fadeIn(1000);
     $('.n-background').hide();
+    $('.about-container').hide();
+    $('.why-me-container').hide();
+
   });
 
+  $('#why-button').click(function() {
+    $('.why-me-container').fadeIn(1000);
+    $('.n-background').hide();
+    $('.about-container').hide();
+    $('.contact-container').hide();
+  });
 });
