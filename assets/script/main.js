08edbc15ef9d102271bf8dd932f1caddba1e27ee
aideecantu <cantu.aidee@berkeley.edu>
2016-11-27 21:51:00 -0800

    Final Project CSS

---
+++ assets/script/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+$('#about-button').click(function() {
+    $('.about-container').fadeIn(1000);
+    $('.n-background').hide();
+  });
+
+  $('#contact-button').click(function() {
+    $('.contact-container').fadeIn(1000);
+    $('.n-background').hide();
+  });
+
+});
