e47eea971a43cdee82a2d6beb972ebbcf32ea643
aideecantu <cantu.aidee@berkeley.edu>
2016-12-28 00:48:15 -0600

    js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
-	
-	// Question 4
-	// IMPLEMENT "SHOW MODAL" WHEN "CLICK ON LOGIN BUTTON FROM MAIN PAGE" HERE
-	$("#link").click(function(){
-		$("#modal-container").show();
-	});
-	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON SUBMIT BUTTON FROM MODAL BOX" HERE
-	$("#submit").click(function(){
-		$("#modal-container").hide();
-	});
-	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON CANCEL BUTTON FROM MODAL BOX" HERE
-	$("#cancel").click(function(){
-		$("#modal-container").hide();
-	});
-	
-	// Question 5
-	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON MODAL OVERLAY" HERE
-	$("#modal-overlay").click(function(){
-		$("#modal-container").hide()
+	$("#button").click(function(){
+    	$('body, html').animate({scrollTop: 0}, 600);
 	});
 });
